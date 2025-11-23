04f533fd8ff83a2a0ffd26931c3e8646575f351e
Florian Bujupi <bujupi9595@hotmail.de>
2025-11-23 02:15:43 +0100

    Add run_mail_list.cmd helper for scheduled mail-list

---
+++ tools/run_mail_list.cmd
@@ -0,0 +1,3 @@
+@echo off
+cd /d "C:\Users\F. Bujupi\Desktop\Lager\Projekte\Bewerbungsagent"
+"C:\Users\F. Bujupi\Desktop\Lager\Projekte\Bewerbungsagent\.venv\Scripts\python.exe" tasks.py mail-list >> "C:\Users\F. Bujupi\Desktop\Lager\Projekte\Bewerbungsagent\logs\mail-list.log" 2>&1
