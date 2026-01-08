d368ba0c65412a4d6604c06243d0325ff1bf87d0
Bua9595 <bujupi9595@hotmail.de>
2026-01-08 11:01:01 +0100

    Remove unused scripts and notes

--- run_export.py
+++
@@ -1,6 +0,0 @@
-from job_collector import collect_jobs, export_csv, format_jobs_plain
-
-jobs = collect_jobs()
-export_csv(jobs)
-print(format_jobs_plain(jobs, top=20))
-print(f"exported {len(jobs)} jobs")
